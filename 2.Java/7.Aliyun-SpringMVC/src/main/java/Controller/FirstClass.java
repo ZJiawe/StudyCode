6771ed4b2ccd68e2fa455c3872e7566a460632ad
郑嘉伟 <1048549613@qq.com>
2019-03-06 19:52:27 +0800

    SSM基本构造创建

---
+++ 2.Java/7.Aliyun-SpringMVC/src/main/java/Controller/FirstClass.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/main")
+public class FirstClass {
+        @RequestMapping(method = RequestMethod.GET)
+        @ResponseBody
+        public String welcome(){
+            return "test Web";
+        }
+}
